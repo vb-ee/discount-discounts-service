f94f565e0c8c1ef5778b52c48bae39ac053863ed
vb1120 <bekmuradovimprove@gmail.com>
2022-09-30 01:47:27 +0500

    mongodb connection init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DiscountsModule } from './discounts/discounts.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DiscountsModule,
+    MongooseModule.forRoot('mongodb://discounts-service-db:27017/discounts'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
