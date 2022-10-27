00574dce3d5cdc62d8b793e08f42ae71e67ec42d
vb1120 <bekmuradovimprove@gmail.com>
2022-10-27 13:02:11 +0500

    fix image rename bug

--- src/utils/ImageUtil.ts
+++ src/utils/ImageUtil.ts
@@ -1,13 +1,13 @@
-import fs from 'fs';
+import { rename, unlink } from 'fs';
 
 export class ImageUtil {
   public static renameImage(oldPath: string, newPath: string) {
-    fs.rename(oldPath, `${newPath}`, (err) => {
+    rename(oldPath, `${newPath}`, (err) => {
       console.log(err);
     });
   }
 
   public static removeImage(imagePath: string) {
-    fs.unlink(imagePath, (err) => console.log(err));
+    unlink(imagePath, (err) => console.log(err));
   }
 }
